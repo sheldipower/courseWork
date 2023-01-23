ab8b8e9be906491445331eb035f448984ec350b1
Irina <sheldipower@gmail.com>
2023-01-23 22:40:06 +0500

    1

---
+++ src/Employee.java
@@ -0,0 +1,57 @@
+public class Employee {
+    private String surname;
+    private String name;
+    private String patronymic;
+    private int department;
+    private int salary;
+    private static int count = 0;
+   private int id ;
+
+
+    public  Employee (String surname, String name, String patronymic, int department, int salary) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.department = department;
+        this.salary = salary;
+        this.id = count++;
+    }
+    public String getSurname (){
+        return surname;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "id:" + id +
+                        "; Сотрудник: " + surname + name+ patronymic +
+                        "; Зарплата: " + salary +
+                        "; Отдел: " + department +
+                        ";";
+
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[]employee = new Employee[10];
+     employee [0] = new Employee("Иванов", "Иван","Иванович", "1", "45000");
+     employee [1] = new Employee("Пертов", "Петр", "Семенович", "3", "50000");
+     employee [2] = new Employee("Семенова", "Наталья", "Аркадьевна", "5", "75000");
+     employee [3] = new Employee("Корешков", "Артем", "Витальевич", "1", "25000");
+     employee [4] = new Employee("Игнатьева", "Мария", "Анатольенва", "2", "47000");
+     employee [5] = new Employee("Мурзин", "Антон", "Григорьевич", "4", "15000");
+     employee [6] = new Employee("Срамков", "Игнат", "Инакентьевич", "3", "19000");
+     employee [7] = new Employee("Мухаметова", "Наталия", "Львовна", "2","71000");
+     employee [8] = new Employee("Козолова", "Елена", "Павловна", "4", "27000");
+     employee [9] = new Employee("Выдров", "Аркадий", "Викторович", "5", "56000");
+
+
     }
-}
+    }
